fd4fd9c199d470460ff95ad650f3c6426ad12764
stanleyrus <devopsowo@gmail.com>
2024-02-23 11:07:18 +0100

    lesson67

--- Weather_app/main.py
+++ Weather_app/main.py
@@ -1,80 +1,5 @@
-import requests
-import datetime
-from openpyxl import Workbook, load_workbook
-from openpyxl.styles import Font
-from os import path
-
-API_KEY = '73b69dd502289cfcb1322eb34935b90b'
-API_URL = 'https://api.openweathermap.org/data/2.5/weather'
-UNITS = 'metric'
-LANG = 'en'
-FILE_EXCEL = './data.xlsx'
-
-
-def get_date_time(ts, timezone, dt_format="%H:%M:%S"):
-    tz = datetime.timezone(datetime.timedelta(seconds=timezone))
-    return datetime.datetime.fromtimestamp(ts, tz=tz).strftime(dt_format)
-
-
-def get_weather(city_name):
-    params = {
-        "appid": API_KEY,
-        "units": UNITS,
-        "lang": LANG,
-        "q": city_name
-    }
-    try:
-        r = requests.get(API_URL, params=params)
-        return r.json()
-    except:
-        return {"cod": 0, "message": "Failed data access"}
-
-
-def print_weather(data):
-
-    if data['cod'] != 200:
-        print(data['message'])
-        return {}
-    else:
-        sunrise_time = get_date_time(data['sys']['sunrise'], data['timezone'])
-        sunset_time = get_date_time(data['sys']['sunset'], data['timezone'])
-        print(f"""
-        Localization: {data['name']}, Country: {data['sys']['country']}
-        Temreture: {data['main']['temp']} ℃
-        Pressure: {data['main']['pressure']} Pa
-        Humidity: {data['main']['humidity']}%
-        Speed of wind: {data['wind']['speed']} m/sec
-        Weather conditions: {data['weather'][0]['description']}
-        Sunrise: {sunrise_time}
-        Sunset time: {sunset_time}
-        """)
-
-        print("+" * 50)
-        return data
-
-
-def save_excel(data):
-    if data['cod'] == 200:
-        if path.exists(FILE_EXCEL):
-            wb = load_workbook(filename=FILE_EXCEL)
-            ws = wb.active           
-        else:
-            wb = Workbook()
-            ws = wb.active
-            ws.title = "Statistic of requests"
-            ws.append(['Date of request', 'City', 'Temperature'])
-            ft = Font(bold=True, color='0000FFFF')
-            a1 = ws['A1']
-            b1 = ws['B1']
-            c1 = ws['C1']
-            a1.font = b1.font = c1.font = ft
-
-        ws.append([datetime.datetime.now(), f"{data['name']}, {data['sys']['country']}", data['main']['temp']])
-        wb.save(filename=FILE_EXCEL)    
-    else:
-        pass
-
-
+import funcsss as f
+import config
 
 print('*' * 70)
 print(("""* Hi! I'll help you to know weater in every city of the world.
@@ -88,29 +13,7 @@ while True:
         print('See you later :)')
         break
     else:
-        weather = get_weather(q)
+        weather = f.get_weather(q)
         print("+" * 50)
-        print_weather(weather)
-        save_excel(weather)
-
-
-"""
-{'coord': {'lon': -0.1257, 'lat': 51.5085}, 
-'weather': [{'id': 500, 'main': 'Rain', 'description': 'light rain', 'icon': '10d'}], 
-'base': 'stations', 
-'main': {'temp': 11.4, 'feels_like': 11.02, 'temp_min': 9.27, 'temp_max': 12.29, 'pressure': 980, 'humidity': 93}, 
-'visibility': 10000, 
-'wind': {'speed': 4.63, 'deg': 180}, 'rain': {'1h': 1}, 
-'clouds': {'all': 100}, 'dt': 1708598371, 
-'sys': {'type': 2, 'id': 2075535, 'country': 'GB', 'sunrise': 1708585324, 'sunset': 1708622786}, 
-'timezone': 0, 
-'id': 2643743, 
-'name': 'London', 
-'cod': 200}
-
-'country': 'BY', 'sunrise': 1708578963, 'sunset': 1708615856, 'timezone': 10800
-'country': 'GR', 'sunrise': 1708578941, 'sunset': 1708618654, 'timezone': 7200
-"""
-# r = requests.get(API_URL, params=params)
-# # res =
-# print(res['sys']['country'])
+        f.print_weather(weather)
+        f.save_excel(weather)
--- lessons/Rick_Morty.py
+++ lessons/Rick_Morty.py
@@ -3,10 +3,8 @@ import random
 
 # y = random.randint(1, 71)
 
-list_ep = list(range(1, 72))
 
-random.shuffle(list_ep)
 
-print(list_ep)
+random.shuffle(list_ep  := list(range(1, 72)))
 
-# print(f'Ty dolzhen seichas posmotret seriu Rick and Morty nr.{y}')
+print(f'Ty dolzhen seichas posmotret seriu Rick and Morty nr.{list_ep}')
