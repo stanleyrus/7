a3c7c21a70fae5b52dd0a17aa86da080f12abf49
stanleyrus <devopsowo@gmail.com>
2024-02-23 13:53:30 +0100

    lesson_68_1

---
+++ lessons/tk_study/main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title('Stanley Application')
+root.iconbitmap('logo.ico')
+# root.iconphoto(False, tk.PhotoImage(file='lol.png'))
+root.geometry('500x300+700+300')
+
+root.mainloop()
